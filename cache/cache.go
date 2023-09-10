c1ea8a308f8cb934e823e557fd5d6798f7982123
Nico <93839513+nicoandrewss@users.noreply.github.com>
2023-09-10 19:02:02 +0200

    fix: race conditions in cache implementation

--- cache/cache.go
+++ cache/cache.go
@@ -1,12 +1,15 @@
 package cache
 
 import (
+	"sync"
+
 	"github.com/Clarilab/clarimq"
 )
 
 // BasicInMemoryCache is a basic in-memory cache implementation of the clarimq.PublishingCache interface.
 type BasicInMemoryCache struct {
-	store map[string]clarimq.Publishing
+	storeMU sync.Mutex
+	store   map[string]clarimq.Publishing
 }
 
 // NewBasicMemoryCache creates a new in-memory cache.
@@ -18,7 +21,9 @@ func NewBasicMemoryCache() *BasicInMemoryCache {
 
 // Put implements the clarimq.PublishingCache interface.
 func (c *BasicInMemoryCache) Put(pbl clarimq.Publishing) error {
+	c.storeMU.Lock()
 	c.store[pbl.ID()] = pbl
+	c.storeMU.Unlock()
 
 	return nil
 }
@@ -27,25 +32,34 @@ func (c *BasicInMemoryCache) Put(pbl clarimq.Publishing) error {
 func (c *BasicInMemoryCache) PopAll() ([]clarimq.Publishing, error) {
 	data := make([]clarimq.Publishing, 0, len(c.store))
 
+	c.storeMU.Lock()
 	for key, val := range c.store {
 		data = append(data, val)
 
 		delete(c.store, key)
 	}
 
+	c.storeMU.Unlock()
+
 	return data, nil
 }
 
 // Len implements the clarimq.PublishingCache interface.
 func (c *BasicInMemoryCache) Len() int {
+	c.storeMU.Lock()
+	defer c.storeMU.Unlock()
+
 	return len(c.store)
 }
 
 // Flush implements the clarimq.PublishingCache interface.
 func (c *BasicInMemoryCache) Flush() error {
+	c.storeMU.Lock()
 	for key := range c.store {
 		delete(c.store, key)
 	}
+
+	c.storeMU.Unlock()
 
 	return nil
 }
